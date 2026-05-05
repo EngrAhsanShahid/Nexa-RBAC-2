fc6e675b002f8dbb5fd6248e6267584226c21a6c
EngrAhsanShahid <66468439+EngrAhsanShahid@users.noreply.github.com>
2026-05-05 16:47:22 +0500

    ip change

--- app/core/ws_manager.py
+++ app/core/ws_manager.py
@@ -1,6 +1,6 @@
 from app.core.connection_manager import ConnectionManager
 
 connectionmanager = ConnectionManager(
-    bootstrap_server="192.168.100.4:9093",
+    bootstrap_server="192.168.100.66:9093",
     topic="alerts"
 )
